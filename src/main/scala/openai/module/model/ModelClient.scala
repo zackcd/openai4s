3079bd08fa738334c254398dd691c2e42420d753
Zachary Downing <zack_downing@mac.com>
2023-03-07 22:35:51 -0800

    Model docstrings

--- src/main/scala/openai/module/model/ModelClient.scala
+++ src/main/scala/openai/module/model/ModelClient.scala
@@ -8,13 +8,35 @@ import openai.module.model.domain.{GetModelsResponse, Model}
 
 import scala.concurrent.{ExecutionContext, Future}
 
+/** List and describe the various models available in the API. You can refer to
+  * the Models documentation to understand what models are available and the
+  * differences between them.
+  * @see
+  *   https://platform.openai.com/docs/api-reference/models
+  */
 sealed trait ModelClient {
 
   val ResourcePath = "/v1/models"
 
-  def getAll: Future[GetModelsResponse]
+  /** Lists the currently available models, and provides basic information about
+    * each one such as the owner and availability.
+    * @see
+    *   https://platform.openai.com/docs/api-reference/models/list
+    * @return
+    *   The list of models.
+    */
+  def listModels: Future[GetModelsResponse]
 
-  def getById(modelId: String): Future[Model]
+  /** Retrieves a model instance, providing basic information about the model
+    * such as the owner and permissioning.
+    * @see
+    *   https://platform.openai.com/docs/api-reference/models/retrieve
+    * @param modelId
+    *   The ID of the model to use for this request
+    * @return
+    *   The model
+    */
+  def retrieveModel(modelId: String): Future[Model]
 }
 
 object ModelClient {
@@ -24,14 +46,14 @@ object ModelClient {
   )(implicit ec: ExecutionContext): ModelClient =
     new ModelClient {
 
-      def getAll: Future[GetModelsResponse] =
+      def listModels: Future[GetModelsResponse] =
         executeRequest[GetModelsResponse](client)(
           BaseUrl + ResourcePath,
           RequestMethod.Get,
           getHeaders(config)
         )
 
-      def getById(modelId: String): Future[Model] =
+      def retrieveModel(modelId: String): Future[Model] =
         executeRequest[Model](client)(
           BaseUrl + ResourcePath + s"/$modelId",
           RequestMethod.Get,
